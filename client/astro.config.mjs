9d84ab28d43930f6d703c388b790a641e8cc6407
Kuro <hello@kurojifusky.com>
2025-11-26 09:26:43 +0800

    added astro front

---
+++ client/astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()],
+  adapter: cloudflare()
+});
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/kuroji-fusky/gideon/server
+
+go 1.24.3
